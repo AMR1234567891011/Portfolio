6b6bfbc4a9220a046e68132991100585f0fc6251
AMR1234567891011 <111772107+AMR1234567891011@users.noreply.github.com>
2024-08-22 14:44:17 -0500

    hashrouter used now

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
